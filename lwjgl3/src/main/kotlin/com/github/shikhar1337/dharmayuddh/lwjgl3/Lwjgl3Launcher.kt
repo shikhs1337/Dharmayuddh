425284e1d085780c89d04761834edc2f8b8770ff
Shikhar <shikhar1337@gmail.com>
2023-07-09 06:31:44 +0530

    Initial Commit

---
+++ lwjgl3/src/main/kotlin/com/github/shikhar1337/dharmayuddh/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.github.shikhar1337.dharmayuddh.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.github.shikhar1337.dharmayuddh.DharmayuddhGame
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(DharmayuddhGame(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("Dharmayuddh")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
